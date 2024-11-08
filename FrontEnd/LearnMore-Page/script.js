1262edc20065834c0a7909be331b4ff5abb9488b
Falak-Chudasama <152072842+Falak-Chudasama@users.noreply.github.com>
2024-11-08 08:14:42 +0530

    Delete FrontEnd/LearnMore-Page directory

--- FrontEnd/LearnMore-Page/script.js
+++
@@ -1,9 +0,0 @@
-const homeBtn = document.getElementById('home');
-const tryUsBtn = document.getElementById('try-us');
-
-homeBtn.addEventListener('click', () => {
-    window.location.href = '../home.html';
-})
-tryUsBtn.addEventListener('click', () => {
-    window.location.href = '../Authentication-Pages/SignUp-Pages/signup1.html';
-})
